24b17ac0d11488c8ef60b51dd66ce4967f0b46ed
unknown <tejindersingh.cse18@chitkarauniversity.edu.in>
2022-08-21 23:08:47 +0530

    dlx

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const app  =require('./app');
+
+const dotenv = require('dotenv');
+const connectDatabase = require('./config/database');
+
+
+
+dotenv.config({path:"backend/config/config.env"});
+
+connectDatabase();
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`listening on port http://localhost:${process.env.PORT}`);
+})
