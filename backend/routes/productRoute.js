b9956abd2768e4c032ca7eb4bffd24f885c45a2f
Tejinder-singh22 <Tejinder.singh@growthnatives.com>
2022-09-01 11:04:19 +0530

    auth middleware

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -4,11 +4,10 @@ const { isAuthenticatedUser } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.route("/products").get(isAuthenticatedUser,getAllProducts);
+router.route("/products").get(getAllProducts);
 
-router.route("/product/new").post(createProduct);
+router.route("/product/new").post(isAuthenticatedUser,createProduct); /*so before adding user, it goes to middleware auth for checking if token stored in cookies is belongs to valid user or not  */ 
 
-router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails);
-
+router.route("/product/:id").put(isAuthenticatedUser,updateProduct).delete(isAuthenticatedUser, deleteProduct).get(getProductDetails);
 
 module.exports = router
